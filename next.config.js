91e3aec5cef8a4528ff7ca417c6d91f707d28d5e
blocklee <block.lee121@gmail.com>
2023-03-28 23:31:37 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
-module.exports = {
-    basePath: '/myAIGC'
-};
+module.exports = (phase, { defaultConfig }) => {
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig = {
+        /* config options here */
+        basePath: '/myAIGC',
+        assetPrefix: '/myAIGC',
+        exportPathMap: async function () {
+            return {
+                '/': { page: '/' },
+                '/api/generate': { page: '/api/generate' }
+            }
+        }
+    }
+    return nextConfig
+}
